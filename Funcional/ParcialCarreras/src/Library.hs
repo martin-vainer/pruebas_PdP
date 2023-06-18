d15a21db072976470f4136194c4e1379980b77b4
martin vainer <vainermartin@gmail.com>
2023-06-18 01:28:27 -0300

    comienzo parcial carreras

--- Funcional/ParcialCarreras/src/Library.hs
+++ Funcional/ParcialCarreras/src/Library.hs
@@ -3,3 +3,13 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data Auto = Auto {
+    color::String,
+    velocidad::Number,
+    distancia::Number
+} deriving(Show, Eq, Ord)
+
+
+-- Carrera = [(Auto, Number)]
+
